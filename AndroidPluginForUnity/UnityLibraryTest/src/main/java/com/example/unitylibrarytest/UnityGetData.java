67d3cbbdc8a318e53d1b2b95395d2d1a8421f6f8
PyramidHead01 <hugo4444333@gmail.com>
2021-02-10 10:32:43 +0100

    21.02.10-10:32-Empezando proyecto

---
+++ AndroidPluginForUnity/UnityLibraryTest/src/main/java/com/example/unitylibrarytest/UnityGetData.java
@@ -0,0 +1,11 @@
+package com.example.unitylibrarytest;
+
+import android.util.Log;
+
+public class UnityGetData {
+
+    public void GetVariable(String nombre, String contrasena, int puntuacion){
+        Log.d("Info", "El nombre es " + nombre + " con contraseña " + contrasena + " y " + puntuacion + "puntos");
+    }
+
+}
